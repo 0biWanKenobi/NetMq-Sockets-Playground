662f303e44eaa89e3cbcd0aeb9371abf28a9910f
Stefano La Terra Inghilterra <stefano.laterrainghilterra@live.com>
2018-07-29 02:27:45 +0200

    First commit

---
+++ NetMqPlayground.Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetMqClient.Model
+{
+    [Serializable]
+    public class Message
+    {
+        public string Text { get; set; }
+        public int ValueCode { get; set; }
+        public string Type { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
